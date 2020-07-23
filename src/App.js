133219d328c8cadd49440c7418942cc38f780cb1
“Miyauchi Nozomu <hunsoku50m@yahoo.co.jp>
2020-07-23 14:34:07 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,46 +1,29 @@
 import React from 'react';
-import "./App.css";
-import PropTypes from 'prop-types';
 
-// class App extends React.Component {
-//   render() {
-//     return (    
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => {console.log("I am clicked.")}} />
-//     </React.Fragment>
-//     )
-//   }
-// }
+const App = () => (<Counter />)
 
-const App = () => {
-  const profiles = [
-    {name: "James", age: 10}, 
-    {name: "Mike", age: 5},
-    {name: "Jessica", age: 20},
-    {name: "Josef", age: 15}
-  ];
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {count: 0}
+  }
 
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name = {profile.name} age={profile.age} key={index}/>
-      })}
-    </div>
-  )
-}
+  hundlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
 
-const User = (props) => {
-return <div>Hi, I am {props.name}. I am {props.age} years old.</div>
-}
-
-// User.defaultProps = {
-//   age: 1
-// }
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+  hundleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.hundlePlusButton}>+1</button>
+        <button onClick={this.hundleMinusButton}>-1</button>
+      </React.Fragment>);
+    }
+};
 
 export default App;
