0b3dccb7db7c09fb95ff555be0a5b3dfadf6d256
“Miyauchi Nozomu <hunsoku50m@yahoo.co.jp>
2020-07-23 11:15:07 +0900

    change tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div><h1>Hello, World!!</h1></div>
+  return <h1>Hello, World!!</h1>
+  // return React.createElement(
+  //   "h1",
+  //   null,
+  //   "Hello, World"
+  // )
 }
 
 export default App;
